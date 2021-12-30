aef2cfcfec410cee309991cd5944f30d35aa8c8b
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-30 21:22:01 +0700

    import draw and algorithm

--- main.py
+++ main.py
@@ -1,54 +1,7 @@
 import pygame
 import random
-from draw import DrawInfo
-
-
-def draw(draw_info, algo_name, ascending):
-    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
-
-    title = draw_info.LARGE_FONT.render(
-        f"{algo_name} - {'Ascending' if ascending else 'Descending'}", 1, draw_info.GREEN)
-    draw_info.window.blit(
-        title, (draw_info.width/2 - title.get_width()/2, 5))
-
-    controls = draw_info.FONT.render(
-        'R - reset | SPACE - start sorting | A - ascending | D - Descending', 1, draw_info.BLACK)
-    draw_info.window.blit(
-        controls, (draw_info.width/2 - controls.get_width()/2, 45))
-
-    sorting = draw_info.FONT.render(
-        'I - insertion sort | B - bubble sort', 1, draw_info.BLACK)
-    draw_info.window.blit(
-        sorting, (draw_info.width/2 - sorting.get_width()/2, 75))
-
-    draw_list(draw_info)
-    pygame.display.update()
-
-
-def draw_list(draw_info, color_positions={}, clear_bg=False):
-    lst = draw_info.lst
-
-    if clear_bg:
-        clear_rect = (draw_info.SIDE_PAD//2, draw_info.TOP_PAD, draw_info.width -
-                      draw_info.SIDE_PAD, draw_info.height - draw_info.TOP_PAD)
-        pygame.draw.rect(draw_info.window,
-                         draw_info.BACKGROUND_COLOR, clear_rect)
-
-    for i, val in enumerate(lst):
-        x = draw_info.start_x + i * draw_info.block_width
-        y = draw_info.height - (val - draw_info.min_val) * \
-            draw_info.block_height
-
-        color = draw_info.GRADIENTS[i % 3]
-
-        if i in color_positions:
-            color = color_positions[i]
-
-        pygame.draw.rect(draw_info.window, color,
-                         (x, y, draw_info.block_width, draw_info.height))
-
-    if clear_bg:
-        pygame.display.update()
+from draw import DrawInfo, draw
+from algorithm import bubble_sort, insertion_sort
 
 
 def generate_starting_list(n, min_val, max_val):
@@ -57,46 +10,6 @@ def generate_starting_list(n, min_val, max_val):
     for _ in range(n):
         val = random.randint(min_val, max_val)
         lst.append(val)
-
-    return lst
-
-
-def bubble_sort(draw_info, ascending=True):
-    lst = draw_info.lst
-
-    for i in range(len(lst) - 1):
-        for j in range(len(lst) - 1 - i):
-            num1 = lst[j]
-            num2 = lst[j + 1]
-
-            if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
-                lst[j], lst[j + 1] = lst[j + 1], lst[j]
-                draw_list(
-                    draw_info, {j: draw_info.GREEN, j + 1: draw_info.RED}, True)
-                yield True
-
-    return lst
-
-
-def insertion_sort(draw_info, ascending=True):
-    lst = draw_info.lst
-
-    for i in range(1, len(lst)):
-        current = lst[i]
-
-        while True:
-            ascending_sort = i > 0 and lst[i - 1] > current and ascending
-            descending_sort = i > 0 and lst[i - 1] < current and not ascending
-
-            if not ascending_sort and not descending_sort:
-                break
-
-            lst[i] = lst[i - 1]
-            i = i - 1
-            lst[i] = current
-            draw_list(draw_info, {i - 1: draw_info.GREEN,
-                      i: draw_info.RED}, True)
-            yield True
 
     return lst
 
