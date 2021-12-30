0f439537aa97673eb91e79dd9eb8babc77e87b7c
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-30 22:11:49 +0700

    fix later merge sort function

--- algorithm.py
+++ algorithm.py
@@ -39,3 +39,40 @@ def insertion_sort(draw_info, ascending=True):
             yield True
 
     return lst
+
+
+def merge_sort(draw_info, ascending=True):
+    lst = draw_info.lst
+
+    if len(lst) > 1:
+        left_lst = lst[:len(lst)//2]
+        right_lst = lst[len(lst)//2:]
+
+        # Recursion
+        merge_sort(left_lst)
+        merge_sort(right_lst)
+
+        # Merge
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_lst) and j < len(right_lst):
+            if left_lst[i] < right_lst[i]:
+                lst[k] = left_lst[i]
+                i += 1
+            else:
+                lst[k] = right_lst[j]
+                j += 1
+            k += 1
+
+        while i < len(left_lst):
+            lst[k] = left_lst[i]
+            i += 1
+            k += 1
+
+        while j < len(right_lst) - 1:
+            lst[k] = right_lst[j]
+            j += 1
+            k += 1
+
+        draw_list(draw_info, {}, True)
